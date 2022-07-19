03462419239b09172870ac24da0de499236a112b
Pekoqs <74448954+Fedve@users.noreply.github.com>
2022-07-20 05:11:03 +0800

    commitchanges

---
+++ index.js
@@ -0,0 +1,123 @@
+const data = [
+  {
+    name: "ABC", 
+    age: "2Years", 
+    child: "DEF", 
+    parent: "GHI"
+  }, 
+  {
+    name: "ABC", 
+    age: "4 Months", 
+    child: "DEF", 
+    parent: "GHI"
+  }, 
+  {
+    name: "ABC", 
+    age: "2Years", 
+    child: "DEF", 
+    parent: "GHI"
+  }, 
+  {
+    name: "ABC", 
+    age: "4 Months", 
+    child: "DEF", 
+    parent: "GHI"
+  }, 
+  {
+    name: "ABC", 
+    age: "2Years", 
+    child: "DEF", 
+    parent: "GHI"
+  }, 
+  {
+    name: "ABC", 
+    age: "4 Months", 
+    child: "DEF", 
+    parent: "GHI"
+  }, 
+  {
+    name: "ABC", 
+    age: "2Years", 
+    child: "DEF", 
+    parent: "GHI"
+  }, 
+  {
+    name: "ABC", 
+    age: "4 Months", 
+    child: "DEF", 
+    parent: "GHI"
+  }, 
+  {
+    name: "ABC", 
+    age: "2Years", 
+    child: "DEF", 
+    parent: "GHI"
+  }, 
+  {
+    name: "ABC", 
+    age: "4 Months", 
+    child: "DEF", 
+    parent: "GHI"
+  }, 
+  {
+    name: "ABC", 
+    age: "2Years", 
+    child: "DEF", 
+    parent: "GHI"
+  }, 
+  {
+    name: "ABC", 
+    age: "4 Months", 
+    child: "DEF", 
+    parent: "GHI"
+  }, 
+  {
+    name: "ABC", 
+    age: "2Years", 
+    child: "DEF", 
+    parent: "GHI"
+  }, 
+  {
+    name: "ABC", 
+    age: "4 Months", 
+    child: "DEF", 
+    parent: "GHI"
+  }, 
+  {
+    name: "ABC", 
+    age: "2Years", 
+    child: "DEF", 
+    parent: "GHI"
+  }, 
+  {
+    name: "ABC", 
+    age: "4 Months", 
+    child: "DEF", 
+    parent: "GHI"
+  }, 
+  {
+    name: "ABC", 
+    age: "2Years", 
+    child: "DEF", 
+    parent: "GHI"
+  }, 
+  {
+    name: "ABC", 
+    age: "4 Months", 
+    child: "DEF", 
+    parent: "GHI"
+  }, 
+  {
+    name: "ABC", 
+    age: "2Years", 
+    child: "DEF", 
+    parent: "GHI"
+  }, 
+  {
+    name: "ABC", 
+    age: "4 Months", 
+    child: "DEF", 
+    parent: "GHI"
+  }
+];
+console.log(data)
